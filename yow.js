dd6c11daebe15e087c7af7b9173f49d7cb99827a
meg768 <magnus@egelberg.se>
2016-05-14 23:54:02 +0200

    -

---
+++ yow.js
@@ -0,0 +1,18 @@
+
+module.exports.isType     = require('./src/istype.js').isType;
+module.exports.isArray    = require('./src/istype.js').isArray;
+module.exports.isNumber   = require('./src/istype.js').isNumber;
+module.exports.isString   = require('./src/istype.js').isString;
+module.exports.isDate     = require('./src/istype.js').isDate;
+module.exports.isObject   = require('./src/istype.js').isObject;
+module.exports.isFunction = require('./src/istype.js').isFunction;
+
+module.exports.rand       = require('./src/random.js').rand;
+module.exports.choose     = require('./src/random.js').choose;
+	
+module.exports.sprintf    = require("sprintf-js").sprintf; 
+module.exports.extend     = require('extend'); 
+
+module.exports.Events     = require('./src/events.js'); 
+
+
