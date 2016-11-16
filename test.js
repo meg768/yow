e43424bc8a9d8ff87f33a7c69228b5faf39a3431
meg768 <magnus@egelberg.se>
2016-11-16 11:21:00 +0100

    -

--- test.js
+++ test.js
@@ -1,13 +1,23 @@
-var random = require('./yow.js').random;
-var fileExists = require('./yow.js').fileExists;
-var writeJSON = require('./yow.js').writeJSON;
-var readJSON = require('./yow.js').readJSON;
 
-console.log(random({A:'Ahh', B:'Bee', C:'See'}));
-console.log(random(['A', 'B', 'C']));
-console.log(random(100, 101));
-console.log(random(1));
-console.log(random('Foo'));
-console.log(fileExists('test.js'));
-console.log(writeJSON('a.json', {a:1}));
-console.log(readJSON('a.json'));
+
+
+var Queue = require('./queue.js');
+
+var queue = new Queue();
+
+function print(text) {
+	return new Promise(function(resolve, reject) {
+		console.log(text);
+		resolve();
+	});
+}
+
+queue.enqueue(print.bind(null, 'Hej'));
+queue.enqueue(print.bind(null, 'Då!'));
+
+queue.dequeue().then(function() {
+	console.log('Klar!');
+})
+.catch(function(error) {
+	console.log(error);
+});
