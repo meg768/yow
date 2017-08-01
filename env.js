77757ae303a213d90e374342cdf781b9cca66660
meg768 <meg768@users.noreply.github.com>
2017-08-01 23:52:15 +0200

    -

---
+++ env.js
@@ -0,0 +1,16 @@
+
+
+function Foo (fileName) {
+
+
+	var Path = require('path');
+
+	var fileParts = Path.parse(fileName);
+	var configFileName = Path.join(fileParts.dir, fileParts.name) + '.env';
+	console.log(configFileName);
+
+	require('dotenv').config(configFileName);
+//	dotenv.config(configFileName);
+}
+
+module.exports = Foo;
