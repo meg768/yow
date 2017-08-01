40ad71a3e17a921a0d0261b9b3d4450d73f3d3c1
meg768 <meg768@users.noreply.github.com>
2017-08-02 01:16:08 +0200

    -

--- config.js
+++ config.js
@@ -3,16 +3,19 @@
 var Config = module.exports = function (fileName) {
 
 	var Path = require('path');
-
 	var fileParts = Path.parse(fileName);
 	var configFileName = Path.join(fileParts.dir, fileParts.name) + '.json';
 
+	var json = {};
+
 	try {
-		console.log('Reading fkle', configFileName);
-		return JSON.parse(fs.readFileSync(fileName));
+		console.log('Reading file', configFileName);
+		json = JSON.parse(fs.readFileSync(fileName));
 
 	}
 	catch(error) {
-		return {};
+		json =  {error:error.messageo};
 	}
+
+	return json;
 }
