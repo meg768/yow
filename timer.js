4c40b653df26429ceba7094f52cfa9145f5a5b2d
meg768 <meg768@users.noreply.github.com>
2018-04-07 23:12:44 +0200

    -

--- timer.js
+++ timer.js
@@ -4,24 +4,26 @@ var Timer = module.exports = function() {
 	var _this = this;
 	var _timer = undefined;
 
-	_this.cancel = function() {
+	function cancel() {
 		if (_timer != undefined)
 			clearTimeout(_timer);
 
 		_timer = undefined;
-	};
+	}
 
-	_this.setTimer = function(delay, fn) {
+	function setTimer(delay, fn) {
 		if (delay == undefined)
 			delay = 3000;
 
-		_this.cancel();
+		cancel();
 
 		_timer = setTimeout(function() {
-			_this.cancel();
+			cancel();
 			fn();
 		}, delay);
+	}
 
-	};
+	_this.cancel = cancel();
+	_this.setTimer = setTimer();
 
 };
