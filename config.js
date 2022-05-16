e1417426452a97314ed8525ed1b27def3a805dfb
meg768 <meg768@users.noreply.github.com>
2022-05-16 20:29:21 +0200

    -

--- config.js
+++ config.js
@@ -1,25 +1,30 @@
+const fs = require('fs');
+const Path = require('path');
 
+module.exports = function loadConfig(fileName) {
 
-var config = {};
+	let configFile = '';
+	let config = {};
 
-function loadConfig(fileName) {
-
-	var json = {};
+	if (fileName == undefined)
+		fileName = '.config';
 
 	try {
-		var fs = require('fs');
-		json = JSON.parse(fs.readFileSync(fileName));
+		if (!fs.existsSync(configFile)) {
+			configFile = Path.join(Path.dirname(process.argv[1]), fileName);
+		}
+	
+		if (!fs.existsSync(configFile)) {
+			configFile = Path.resolve(process.cwd(), fileName);
+		}
+		
+		if (fs.existsSync(configFile)) {
+			config = JSON.parse(fs.readFileSync(configFile));	
+		}
 	}
 	catch(error) {
-		console.error(error);
+		config.error = error;
 	}
 
-	json.load = loadConfig;
-
-	return json;
+	return config;
 }
-
-var config = loadConfig('.config');
-
-
-module.exports = config;
